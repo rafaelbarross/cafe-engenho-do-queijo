03f57b227681d49c3cae281a7c5bc3c68081db4c
Rafael Barros <rsb8@aluno.ifal.edu.br>
2025-01-29 14:29:39 -0300

    fix: atualiza links e melhora a responsividade no componente RedeSociais

--- src/app/components/redes-sociais.tsx
+++ src/app/components/redes-sociais.tsx
@@ -8,21 +8,31 @@ import { RiInstagramFill } from "react-icons/ri";
 export default function RedeSociais() {
   return (
     <div className="flex items-center gap-2">
-      <Link passHref href="https://www.instagram.com/cafeengenhodoqueijo" target="_blank" referrerPolicy="no-referrer">
+      <Link
+        passHref
+        href="https://www.instagram.com/cafeengenhodoqueijo"
+        target="_blank"
+        referrerPolicy="no-referrer"
+      >
         <Button
           variant="default"
           size="icon"
-          className="rounded-full !p-0 h-8 w-8"
+          className="rounded-full !p-0 h-8 w-8 hover:scale-110 transition-all"
         >
           <RiInstagramFill />
         </Button>
       </Link>
 
-      <Link passHref href="" target="_blank" referrerPolicy="no-referrer">
+      <Link
+        passHref
+        href="https://www.facebook.com/p/Caf%C3%A9-Engenho-do-Queijo-100075888760204/"
+        target="_blank"
+        referrerPolicy="no-referrer"
+      >
         <Button
           variant="default"
           size="icon"
-          className="rounded-full !p-0 h-8 w-8"
+          className="rounded-full !p-0 h-8 w-8  hover:scale-110 transition-all"
         >
           <FaFacebook />
         </Button>
